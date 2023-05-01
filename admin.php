a1194979512e05b9fb87c99def54f50e1416cfca
Butch Castro <butchadrianc@gmail.com>
2023-05-01 22:32:28 +0800

    Added Login page and admin page

--- admin.php
+++ admin.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-
-
     <title>Commerce App</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
 </head>
+
 <body>
-    
+    <div class="container">
+        <h1>Admin Page</h1>
+
+        <div class="buttons">
+            <button class="form-btn">Add Product</button>
+            <button class="form-btn">Edit Product</button>
+            <button class="form-btn">Remove Product</button>
+        </div>
+    </div>
+
+
 </body>
+
 </html>
---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Commerce App</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Login Page</h1>
+        <div class="container-forms">
+            <p>Username:</p>
+            <input type="text">
+            <p>Password:</p>
+            <input type="password">
+        </div>
+
+        <div class="buttons">
+            <a href="admin.php"><button class="form-btn">Login</button></a>
+            <button class="form-btn">Sign Up</button>
+            <button class="form-btn">Admin Login</button>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
